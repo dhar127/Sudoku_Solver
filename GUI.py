64730ae9a577764360f56c31e8e29cac03cd0a29
dhar127 <dhar22028.cs@rmkec.ac.in>
2023-09-06 21:57:33 +0530

    changed

--- GUI.py
+++ GUI.py
@@ -1,16 +1,8 @@
 import time
 import pygame
 import pygame.event
-#import pygame.key
 from pygame.locals import *
-'''from pygame.key import (
-    MOUSEBUTTONDOWN, QUIT,KEYDOWN,K_1,K_2,K_3,K_4,K_5,K_6,K_7,K_8,K_9,K_KP1,K_KP2,K_KP3,K_KP4,K_KP5,K_KP6,K_KP7,K_KP8,K_KP9,K_DELETE,K_SPACE,K_RETURN
-)'''
-
-
 pygame.font.init()
-
-
 class Grid:
     board = [
         [7, 8, 0, 4, 0, 0, 1, 2, 0],
